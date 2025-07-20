e55fab0e18745f0c9da043efab271638cc1be114
adewale <adewale.d.a@outlook.com>
2025-07-20 20:55:50 +0100

    Full payment webhook restoration

--- src/app/api/payment-gateway/webhook/route.ts
+++ src/app/api/payment-gateway/webhook/route.ts
@@ -37,15 +37,10 @@ export async function POST(req: NextRequest) {
           amount: data.amount / 100,
           metaData: data.metadata, //order_id, user_id , payment_type : "order" | "vendor_debt" | "aod_subscription"
         };
-
         if (result?.metaData?.payment_type === "order") {
           const order_id = result?.metaData?.order_id;
-          const {
-            allow_order_notification,
-            error_message,
-            sender_name,
-            sender_phone,
-          } = await handleOrderPayment({ supabase, result });
+          const { allow_order_notification, sender_name, sender_phone } =
+            await handleOrderPayment({ supabase, result });
           if (allow_order_notification) {
             // 6. Send notifications
             await resend.emails.send({
@@ -54,19 +49,20 @@ export async function POST(req: NextRequest) {
               subject: "We have received your order",
               react: OrderCreatedEmail({ orderId: order_id }),
             });
+
+            // 7. Send notification to user
+            await resend.emails.send({
+              from: "Cartway Website <website@cartwayhq.com>",
+              to: "adewale.d.a@outlook.com", //"orders@cartwayhq.com",
+              subject: "🔔New Order Alert",
+              react: NewOrderAlertEmail({
+                orderId: order_id,
+                name: sender_name,
+                phone: sender_phone,
+                email: customerEmail,
+              }),
+            });
           }
-          // 7. Send notification to user
-          await resend.emails.send({
-            from: "Cartway Website <website@cartwayhq.com>",
-            to: "adewale.d.a@outlook.com", //"orders@cartwayhq.com",
-            subject: "🔔New Order Alert",
-            react: NewOrderAlertEmail({
-              orderId: `order_id - ${error_message}`,
-              name: sender_name,
-              phone: sender_phone,
-              email: `adewale.d.a@outlook.com - ${JSON.stringify(data)}`,
-            }),
-          });
         } else if (
           result?.metaData?.payment_type === "vendor_debt" ||
           result?.metaData?.payment_type === "vendor_debt_dva"
